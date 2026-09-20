a0e2f7397e872d61ca0a9fc1acac8f6d3cc5c652
Thoufiq Ahamed <thoufiq.ahamed2201@gmail.com>
2026-09-20 16:44:11 +0530

    Fix exact alignment and spacing for Navbar search and menus based on reference

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
           <span className="divider"></span>
           <button className="search-btn">Anytime</button>
           <span className="divider"></span>
-          <button className="search-btn search-add-guests" style={{lineHeight: 1.2, paddingRight: '8px'}}>Add<br/>guests</button>
+          <button className="search-btn search-add-guests" style={{paddingRight: '8px'}}>Add guests</button>
           <div className="search-icon-wrapper">
             <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" style={{display: 'block', fill: 'none', height: '12px', width: '12px', stroke: 'currentcolor', strokeWidth: '5.33333', overflow: 'visible'}}>
               <g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g>
@@ -51,7 +51,7 @@ const Navbar = () => {
 
         {/* User Menu */}
         <div className="navbar-user-menu flex items-center gap-2">
-          <a href="#" className="host-link" style={{lineHeight: 1.2, padding: '12px 8px'}}>Become a<br/>host</a>
+          <a href="#" className="host-link" style={{padding: '12px'}}>Become a host</a>
           <button className="globe-btn flex items-center justify-center">
              <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" style={{display: 'block', height: '16px', width: '16px', fill: 'currentcolor'}}><path d="M8 .25a7.77 7.77 0 0 1 7.75 7.78 7.75 7.75 0 0 1-7.52 7.72h-.25A7.75 7.75 0 0 1 .25 8.24v-.25A7.75 7.75 0 0 1 8 .25zm1.95 8.5h-3.9c.15 2.9 1.17 5.34 1.88 5.5H8c.68 0 1.72-2.37 1.93-5.23zm4.26 0h-2.76c-.09 1.96-.53 3.78-1.18 5.08A6.26 6.26 0 0 0 14.17 8.75zm-9.67 0H1.8a6.26 6.26 0 0 0 2.76 5.08 12.6 12.6 0 0 1-1.14-5.08zm4.51-7c-.71.16-1.73 2.61-1.88 5.5h3.9c-.15-2.9-1.17-5.34-1.88-5.5zm4.46 1.92a12.6 12.6 0 0 1 1.14 5.08h2.76a6.26 6.26 0 0 0-2.76-5.08zM1.8 7.25h2.76a12.6 12.6 0 0 1 1.14-5.08A6.26 6.26 0 0 0 1.8 7.25z"></path></svg>
           </button>
